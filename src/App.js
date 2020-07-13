42bf3f847b3ebd53cfc61c9946fc8abd3490519e
Robert Parker <diaboloshogunate@gmail.com>
2020-07-12 19:05:59 -0700

    update build script

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Updated text
+          Updated text again
         </p>
         <a
           className="App-link"
