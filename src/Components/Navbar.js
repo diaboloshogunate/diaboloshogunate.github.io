0d92f670cd1bde6b6b4f7c324fa578950067076f
Robert Parker <diaboloshogunate@gmail.com>
2020-08-23 21:33:36 -0700

    remove unused nav

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,17 +1,11 @@
 import React, {Component} from "react";
-import {Container, Nav, Navbar as NBar} from 'react-bootstrap'
+import {Container, Navbar as NBar} from 'react-bootstrap'
 
 class Navbar extends Component {
     render() {
         return <NBar bg="primary" variant="light" expand="lg" sticky="top">
             <Container>
                 <NBar.Brand href="#home">Robert Parker</NBar.Brand>
-                {/*<NBar.Toggle aria-controls="basic-navbar-nav"/>*/}
-                {/*<NBar.Collapse id="basic-navbar-nav">*/}
-                {/*    <Nav className="ml-auto">*/}
-                {/*        <Nav.Link href="/">Home</Nav.Link>*/}
-                {/*    </Nav>*/}
-                {/*</NBar.Collapse>*/}
             </Container>
         </NBar>
     }
